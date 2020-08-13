88869e2d747e3e8492af1981cf4ee2b4b029dada
genadigeno <genadimumladze44@gmail.com>
2020-08-13 14:47:42 +0400

    no message

---
+++ src/main/java/geno/learn/jenkins/Configuration.java
@@ -0,0 +1,8 @@
+package geno.learn.jenkins;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@org.springframework.context.annotation.Configuration
+public class Configuration extends WebMvcConfigurationSupport {
+    
+}

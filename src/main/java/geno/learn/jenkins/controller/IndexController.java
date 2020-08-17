2165aad74853fe9ce6cb34e39795964c83af88fe
genadigeno <genadimumladze44@gmail.com>
2020-08-17 14:30:37 +0400

    no message

---
+++ src/main/java/geno/learn/jenkins/controller/IndexController.java
@@ -0,0 +1,16 @@
+package geno.learn.jenkins.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ResponseEntity<?> index(){
+        System.out.println("Ok");
+        return ResponseEntity.ok("Ok");
+    }
+}

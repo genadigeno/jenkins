3315ece9627c4a1b40d69866933558ad42ff314b
genadigeno <genadimumladze44@gmail.com>
2021-01-09 13:02:19 +0400

    add

--- src/test/java/geno/learn/jenkins/Simple Test.java
+++ src/test/java/geno/learn/jenkins/Simple Test.java
@@ -1,9 +1,7 @@
 package geno.learn.jenkins;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class JenkinsApplicationTests {
 
 	@Test
